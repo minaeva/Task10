8f1d895499b29fa0a84a01c14e4686d286cf433d
Sveta Minaeva <s.minaeva@gmail.com>
2019-05-23 15:28:29 +0300

    DataSource configuration files created; DAO layer switched to lookup DataSource by JNDI name.

--- src/main/java/com/foxminded/dao/DaoConnection.java
+++ src/main/java/com/foxminded/dao/DaoConnection.java
@@ -1,37 +1,45 @@
 package com.foxminded.dao;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.Properties;
 import org.apache.log4j.Logger;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
 
 public class DaoConnection {
 
     private static final Logger log = Logger.getLogger(DaoConnection.class);
 
-    private static Properties properties = null;
+    private static Connection connection;
 
-    static public Connection getConnection(){
-        if (properties == null){
-            properties = DaoProperties.read();
-            try {
-                log.trace("Initializing JDBC driver");
-                Class.forName(properties.getProperty("db.postgre.driver"));
-            } catch (ClassNotFoundException ex){
-                log.error("Cannot initialize JDBC driver ", ex);
-                throw new DaoException("Cannot initialize JDBC driver ", ex);
-            }
-        }
+    public static Connection getConnection() {
+        Context context;
         try {
-            log.trace("Establishing postgres connection");
-            return DriverManager.getConnection(
-                    properties.getProperty("db.postgre.url"),
-                    properties.getProperty("db.postgre.login"),
-                    properties.getProperty("db.postgre.password"));
-        } catch (SQLException ex) {
-            log.error("Cannot establish postgres connection ", ex);
-            throw new DaoException("Cannot establish postgres connection ", ex);
+            log.trace("Initializing context");
+            context = new InitialContext();
+            if (context == null) {
+                log.error("Initial context is null");
+                throw new DaoException("Initial context is null");
+            }
+            log.trace("Initializing data source");
+            DataSource dataSource = (DataSource) context.lookup("java:/comp/env/jdbc/postgres");
+            if (dataSource == null) {
+                log.error("Data source is null");
+                throw new DaoException("Data source is null");
+            }
+            try {
+                log.trace("Establishing postgres connection");
+                connection = dataSource.getConnection();
+            } catch (SQLException e) {
+                log.error("Cannot establish postgres connection ", e);
+                throw new DaoException("Cannot establish postgres connection ", e);
+            }
+        } catch (NamingException e) {
+            log.error("Cannot initialize context or data source ", e);
+            throw new DaoException("Cannot initialize context or data source ", e);
         }
+        return connection;
     }
 }
